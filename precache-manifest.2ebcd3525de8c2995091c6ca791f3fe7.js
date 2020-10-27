f491a97fcf9098b0db3a1f7d82959db6ae279a07
Liza <elizabethhoncharenko@gmail.com>
2020-10-27 12:35:16 +0200

    Updates

---
+++ precache-manifest.2ebcd3525de8c2995091c6ca791f3fe7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cf755f5448909d65a0ba33217fbb533",
+    "url": "./index.html"
+  },
+  {
+    "revision": "eb8b5d7b8d658a1ab4ae",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "9bfabb54970d8a1edebf",
+    "url": "./static/js/2.c17dec48.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c17dec48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb8b5d7b8d658a1ab4ae",
+    "url": "./static/js/main.339214ec.chunk.js"
+  },
+  {
+    "revision": "89de80f3985b25002b5f",
+    "url": "./static/js/runtime-main.17b9eccd.js"
+  }
+]);
